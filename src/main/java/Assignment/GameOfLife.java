f2b45d09e871b9323cbb099fd36f87da709eb67f
Shubham Yadav <yshubham1898@gmail.com>
2022-07-30 23:40:52 +0530

    added game entry code

--- src/main/java/Assignment/GameOfLife.java
+++ src/main/java/Assignment/GameOfLife.java
@@ -2,66 +2,41 @@ package Assignment;
 
 public class GameOfLife {
 
-    public static void main(String[] args) {
-        int[][] input = {{1,1},{1,0}};
+    public static void main(String[] args) throws InterruptedException {
+        boolean[][] randomBoard = getRandomBoard();
+        Board board = new Board(randomBoard);
+        System.out.println("Generating Random board!!\n");
 
-        int[][] result = nextIteration(input);
+        Thread.sleep(1000);
 
+        System.out.println("Initial Generation!!");
+        board.showBoard();
+        System.out.println("\n");
+        while (board.isGameCompleted()) {
+            board.calculateNextGenerationOfBoard();
+            if (board.isGameCompleted()) {
+                System.out.println("Next Generation!!");
+                board.showBoard();
+                System.out.println("\n");
+                Thread.sleep(1000);
+            }
+        }
+        System.out.println("Game completed !!");
     }
 
 
-    static int[][] nextIteration(int matrix[][]) {
-
-        int M = matrix.length;
-        int N = matrix[0].length;
-        int[][] futureMatrixShape = new int[M][N];
-
-        int[][] positions = {{-1, 0},
-                {-1, 1},
-                {0, 1},
-                {1, 1},
-                {1, 0},
-                {1, -1},
-                {0, -1},
-                {-1, -1}
-        };
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-
-                int alive = 0;
-
-                for (int[] position : positions) {
-
-                    int x = position[0] + i;
-                    int y = position[1] + j;
-
-                    if (x >= 0 && x < M && y >= 0 && y < N && matrix[x][y] == 1) {
-                        alive++;
-                    }
-                }
-
-                //here if dead and 3 neighbour are alive, than dead ==> alive
-                if (matrix[i][j] == 0 && alive == 3) {
-                    futureMatrixShape[i][j] = 1;
-                    //here if alive
-                } else if (matrix[i][j] == 1) {
-
-                    //if alive and 2 or 3 neighbours are alive than stays alive
-                    if (alive == 2 || alive == 3) {
-                        futureMatrixShape[i][j] = 1;
-                    }
-                }
+    private static boolean[][] getRandomBoard() {
+        int m = 5;
+        int n = 5;
+        boolean[][] randomBoard = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                randomBoard[i][j] = Math.random() > 0.5;
             }
         }
 
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                matrix[i][j] = futureMatrixShape[i][j];
-            }
-        }
-        return matrix;
+        return randomBoard;
     }
 
 
